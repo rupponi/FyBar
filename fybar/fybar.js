deca983b17b7bd25e1f6284863bd32af6bbcb767
Rohan Upponi <rponi70@gmail.com>
2019-03-24 02:43:49 -0700

    Update: - Successful authorization. Need to continue with acquirable token to assign   functions to buttons for controlling playback now that we have playback   control permissions.

--- fybar/fybar.js
+++ fybar/fybar.js
@@ -1,16 +1,38 @@
-const app = {};
+const client_id = '85a6448ca4f841c5a6566d782c9bc50b';
+const scopes = 'user-modify-playback-state';
+const redirect_uri = 'https://rohanupponi.com';
+let me = null;
 
-app.get('/login', function(req, res) {
-    var scopes = 'user-read-private user-read-email';
-    res.redirect('https://accounts.spotify.com/authorize' +
-        '?response_type=code' +
-        '&client_id=' + my_client_id +
-        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-        '&redirect_uri=' + encodeURIComponent(redirect_uri));
-});
+let authRequestWindow;
 
-app.init=(e)=>{
-    
-};
+function authorize() {
+    authRequestWindow = window.open(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`, 'Login with Spotify', 'width=600,height=400');
 
-$(app.init);
+    window.spotifyCallback = (payload) => {
+        authRequestWindow.close();
+        fetch('https://api.spotify.com/v1/me', {
+            headers: {
+                'Authorization': `Bearer ${payload}`
+            }
+        }).then(response => {
+            me = response.headers.toString();
+            return response.json();
+        }).then(data => {
+            console.log(me);
+        });
+    }
+}
+
+function hello() {
+    console.log("Hello world!");
+    console.log(me);
+}
+
+function mounted() {
+    this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+
+    if (this.token) {
+        window.spotifyCallback(this.token);
+    }
+}
+
